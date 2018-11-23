75821465957c4090d6405818fd17b167637f7f61
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2018-11-23 08:33:38 +0000

    Testing with react-test-renderer and jest . Filed bug report as error occurs on testing. Left here for review

--- src/components/App/app.test.js
+++ src/components/App/app.test.js
@@ -1,7 +1,7 @@
 import renderer from 'react-test-renderer'
 import App from './App'
 
-test('The <APP /> component is corevtly rendered', () => {
+test('The <APP /> component is correctly rendered', () => {
   const component = renderer.create(<App />)
   let tree = component.toJSON()
   expect(tree).toMatchSnapshot()
